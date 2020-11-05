7453e9c59386061a2c5b087553fe68478e0a3743
Yuncoco-DUO <3392421354@qq.com>
2020-11-05 19:20:05 +0800

    第十四次课

---
+++ 第十四次课/js初步学习.js
@@ -0,0 +1,8 @@
+function orange(){
+    var array=[1,2,3,4,8,6,5,9,7]
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        console.log(element);
+    }
+}
+orange();
